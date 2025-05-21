aaeb65a8bb238f15575a0603008850e45da9859e
Devesh Pandey <90002933@vishakhagroup.com>
2025-05-21 18:31:23 +0530

    Today's commit

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
     return (
-      <div>Header</div>
-    )
+      <>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about-us">About Us</Link> |{" "}
+        <Link to="/contact-us">Contact Us</Link>
+      </>
+    );
   }
 }
