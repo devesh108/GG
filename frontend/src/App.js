502179f731f416076ef4fd1be058313de7368d75
Devesh Pandey <90002933@vishakhagroup.com>
2025-05-27 18:43:21 +0530

    New Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Dashboard from './components/dashboard/Dashboard';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="about-us" element={<About />} />
-        <Route path="contact-us" element={<Contact />} />
-      </Routes>
-      <Footer />
-
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
-
-
--- frontend/src/components/pagenotfound/Pagenotfound.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Pagenotfound extends Component {
-  render() {
-    return (
-      <div>Pagenotfound</div>
-    )
-  }
-}
