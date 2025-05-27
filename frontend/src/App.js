b2fa4404cc471e6e80b0573820f576e892cba799
Devesh Pandey <90002933@vishakhagroup.com>
2025-05-27 19:41:40 +0530

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Router, Routes} from 'react-router-dom';
+import {Header} from './components/header/Header';
+import {Footer} from './components/footer/Footer';
+import {Dashboard} from './components/dashboard/Dashboard';
+import {About} from './components/about/About';
+import {Contact} from './components/contact/Contact';
+import {pagenotfound} from './components/pagenotfound/Pagenotfound'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+
   );
 }
 
